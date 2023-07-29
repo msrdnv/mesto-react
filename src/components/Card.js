22d206f8c524b06e417b67da0ea3713f80b9fc57
msrdnv <msrdnv@ya.ru>
2023-07-29 17:21:55 +0300

    hotfix: gh-pages

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ export default function Card (props) {
   return (
     <>
       <button className="element__delete-button" type="button"></button>
-      <img className="element__image" src={props.card.link} onClick={handleClick}/>
+      <img className="element__image" src={props.card.link} alt="Фотография из галереи" onClick={handleClick}/>
       <h2 className="element__name">{props.card.name}</h2>
       <div className="element__like-container">
         <button className="element__like-button" type="button"></button>
