9b681e6b071499ebadca90b5552b158af275a109
msrdnv <msrdnv@ya.ru>
2026-01-09 21:01:25 +0300

    migrate cra to vite

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logoMestoWhite from '../images/logo-mesto-white.svg';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logoMestoWhite} alt="Логотип Mesto Russia"/>
+    </header>
+  )
+};
+
+
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App.jsx';
+
+const root = ReactDOM.createRoot(document.querySelector('.page'));
+root.render(
+  <App />
+);
